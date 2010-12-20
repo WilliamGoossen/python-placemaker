f7eaadff2e97b55fc344ce51260971031e1c9b16
snowcloud <derek@snowcloud.co.uk>
2010-12-20 02:21:57 +0000

    added setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='python-placemaker',
+      version='0.1.0',
+      description='Python wrapper for the Yahoo Placemaker API',
+      author='Aaron Bycoffe',
+      author_email='bycoffe@gmail.com',
+      url='http://github.com/bycoffe/python-placemaker/',
+      packages=[],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
